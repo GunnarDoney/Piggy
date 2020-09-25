5fe71bb780b82a53f4ebc0e6a82c11d6f929acab
GunnarDoney <70531380+GunnarDoney@users.noreply.github.com>
2020-09-25 10:42:37 -0400

    Update student.py

--- student.py
+++ student.py
@@ -60,7 +60,7 @@ class Piggy(PiggyParent):
         """A higher-ordered algorithm to make your robot dance"""
         # TODO: check to see if it's safe before dancing
         
-        spin_left_spin_right()
+        self.spin_left_spin_right()
         
     
     def spin_left_spin_right(self):
