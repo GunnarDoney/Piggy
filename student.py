b3ab5544a7653ed4ad03c8f00ccf6916da6a14b1
GunnarDoney <70531380+GunnarDoney@users.noreply.github.com>
2020-10-06 10:29:53 -0400

    started to work on safe distance and head turn

--- student.py
+++ student.py
@@ -19,6 +19,8 @@ class Piggy(PiggyParent):
         '''
         self.LEFT_DEFAULT = 80
         self.RIGHT_DEFAULT = 80
+        self.safe_distance = 300
+        self.close_distance  = 150
         self.MIDPOINT = 1500  # what servo command (1000-2000) is straight forward for your bot?
         self.set_motor_power(self.MOTOR_LEFT + self.MOTOR_RIGHT, 0)
         self.load_defaults()
@@ -161,6 +163,7 @@ class Piggy(PiggyParent):
         pass
 
     def nav(self):
+        """ Auto pilot """
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("-------- [ Press CTRL + C to stop me ] --------\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
